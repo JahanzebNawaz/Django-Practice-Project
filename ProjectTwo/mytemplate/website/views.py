ebfd73cf74d790a121b4d78f341115af6be1d53d
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-07-14 15:24:39 +0500

    ProjectTwo Urls and Tempalteing

---
+++ ProjectTwo/mytemplate/website/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+
+class NavbarPageView(TemplateView):
+    template_name = 'navbar.html'
+
