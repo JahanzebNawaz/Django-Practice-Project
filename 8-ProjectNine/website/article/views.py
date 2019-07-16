e3873d86779a9db682b52d31ed8f8b6a33d08df3
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-07-16 08:13:59 +0500

    Login and Auth in Django

---
+++ 8-ProjectNine/website/article/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView, DetailView
+
+from .models import Article
+# Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'home.html'
+
+
+class ArticleDetailsView(DetailView):
+    model = Article
+    template_name = 'detail.html'
+    context_object_name = 'batman'
